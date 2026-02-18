0d6769f421894487a366e487a3676a6276782dfe
Elvis Chege <96030189+elviscgn@users.noreply.github.com>
2026-02-18 13:41:55 +0200

    visual progress

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Inspired by: https://www.youtube.com/watch?v=qjWkNZ0SXfo but in java
 `(x, y, z)`\
 `x' = x/z`\
 `y' = y/z`
+
+Progress
+![progress.png](progress.png)
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("2D Graphics");
+        JFrame frame = new JFrame("3D Graphics Render");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 500);
         frame.setLocationRelativeTo(null);
@@ -13,7 +13,7 @@ public class Main {
         frame.setVisible(true);
 
         panel.clearScreen();
-        panel.drawPoint(0, -5);
+        panel.drawPoint(0, 0);
 //        panel.point(200, 200);
 //        panel.point(300, 100);
 
